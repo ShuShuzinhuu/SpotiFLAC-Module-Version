69b257e0e0c1e2eb9a4594c6512f315f516345ea
JBG <jbgamer17252@gmail.com>
2026-01-11 20:37:57 +0100

    - test

--- launcher.py
+++ launcher.py
@@ -6,17 +6,16 @@ Launcher script for SpotiFLAC
 import sys
 import os
 
-# Add current directory to path to allow absolute imports
 if getattr(sys, 'frozen', False):
-    # Running as compiled executable
     application_path = sys._MEIPASS
 else:
-    # Running as script
     application_path = os.path.dirname(os.path.abspath(__file__))
 
 sys.path.insert(0, application_path)
 
 # Now import and run the main SpotiFLAC module
 if __name__ == '__main__':
-    import spotiflac.SpotiFLAC
-    spotiflac.SpotiFLAC.main()
+    from spotiflac import spotiflac
+    from spotiflac import SpotiFLAC
+    args = SpotiFLAC.parse_args()
+    spotiflac(args.url, args.output_dir, args.services)
